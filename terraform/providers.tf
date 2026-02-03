4f068721e6b1b2b11c38b00cd950274416eaf42e
marina-a-lopez <malope@edem.es>
2026-02-03 10:49:39 +0100

    Cambios en terraform
    En providers, gitignore y variables

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+
 provider "google" {
     project = var.project_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,20 @@ variable "project_id" {
   description = "ID del proyecto en GCP"
   type = string
 }
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "europe-southwest1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "europe-southwest1-b"
+}
+
+variable "service_account_email" {
+  description = "Email of the service account to attach to the VMs"
+  type        = string
+}
