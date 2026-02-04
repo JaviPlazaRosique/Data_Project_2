db5b1f2d249b52fd0967c03a694aceb5d65d5410
JaviPlazaRosique <jplaz@LAPTOP-0MSP29H6.localdomain>
2026-02-04 13:55:50 +0100

    moldes de los datos api

--- api/main.py
+++ api/main.py
@@ -1,5 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from datetime import date
 import os
+import json
+
+class Menores(BaseModel):
+    nombre: str
+    apellidos: str
+    dni: str
+    fecha_nacimiento: date
+    direccion: str
+    url_foto: str
+    discapacidad: bool
+
+class Adultos(BaseModel):
+    nombre: str
+    apellidos: str
+    telefono: str
+    id_menor: int
+    email: str
+
+class HistoricoUbicaciones(BaseModel):
+    id_menor: int
+    latitud: float
+    longitud: float
+    radio: int
+    fecha: date
+    duracion: int
+
+class ZonasRestringidas(BaseModel):
+    nombre: str
+    latitud: float
+    longitud: float
+    radio_peligro: int
+    radio_advertencia: int
 
 app = FastAPI()
 
+@app.get("/menores")
