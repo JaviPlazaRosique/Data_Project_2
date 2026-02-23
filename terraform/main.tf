bee146f695bebd0f74d2b81ed8354ed5020baa34
Celia <cesaco@edem.es>
2026-02-23 23:18:20 +0100

    ci cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -312,6 +312,7 @@ resource "google_cloud_run_v2_service" "web_cloud_run" {
       ports {
         container_port = 8080
       }
+
       startup_probe {
         initial_delay_seconds = 10
         timeout_seconds = 5
